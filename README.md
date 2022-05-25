ff9a81534b46279e4929c813897c06af90224f46
Damilola Mohammed <damilolamohammed2@hotmail.com>
2022-05-25 15:25:50 +0100

    completed

---
+++ README.md
@@ -0,0 +1,53 @@
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+![](./images/screencapture-calculator-app.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Vanilla JavaScript
+
+### What I learned
+- learned how to use keypress enter event
+
+### Continued development
+- Need to continue mastering page responsiveness
+- Need to master how to use the class functions and OOP
+
+### Useful resources
+- [w3schools](https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_trigger_button_enter) - This helped me learn how to use keypress enter for event
+
+## Author
+
+- Blog - [D.M. Oladele](https://activuscode.hashnode.dev/)
+- Twitter - [@activus_d](https://twitter.com/activus_d)
---
+++ main.js
@@ -0,0 +1,93 @@
+// get value of bill
+// get percentage of tip
+// get number of people
+// get total amount of tip by dividing tip by 100 and multiplying the result by bill
+// get tip per person by dividing total amount of tip by number of people
+// get the total amount by adding total amount of tip with bill
+// get total per person by dividing total amount by number of people
+
+let bill = document.querySelector('#bill')
+let numOfPeople = document.querySelector('#people')
+let displayTip = document.querySelector('.displayTip')
+let displayTotal = document.querySelector('.displayTotal')
+let tips = document.querySelector('.tipBtns')
+let customTip = document.querySelector('#customTip')
+
+tips.addEventListener('click', event => { //add click event to tip buttons to get get value and give output
+    target = event.target;
+    let tip = target.value
+    tipClone = tip
+    !target.matches('button') ? '' : splitter.parseInput(tip)
+})
+
+let keyPressOutput = [bill, customTip, numOfPeople]
+keyPressOutput.forEach( e => {
+    e.addEventListener("keypress", function(event) { // add keypress event to all inputs to get value entered and give output
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+          // Cancel the default action, if needed
+          event.preventDefault();
+          // Trigger the button element with a click
+            splitter.parseInput(Number(customTip.value))
+        }
+      });
+} )
+
+// customTip.addEventListener("keypress", function(event) { // add keypress event to custom tip input to get value entered and give output
+//     // If the user presses the "Enter" key on the keyboard
+//     if (event.key === "Enter") {
+//       // Cancel the default action, if needed
+//       event.preventDefault();
+//       // Trigger the button element with a click
+//         splitter.parseInput(Number(customTip.value))
+//     }
+//   });
+
+const splitter = {
+    parseInput(tip) {
+        if( (bill.value === null || bill.value == 0) && (numOfPeople === null || numOfPeople.value == 0) ) {
+            document.querySelector('.errorBill').textContent = `Can't be zero`
+            document.querySelector('.errorPeople').textContent = `Can't be zero`
+        }else if( bill.value === null || bill.value == 0 ) {
+            document.querySelector('.errorBill').textContent = `Can't be zero`
+        }else if( numOfPeople === null || numOfPeople.value == 0 ) {
+            document.querySelector('.errorPeople').textContent = `Can't be zero`
+        }else{
+            document.querySelector('.errorBill').textContent = ''
+            document.querySelector('.errorPeople').textContent = ''
+            displayTip.textContent = '$' + this.tipPerPerson(tip)
+            displayTotal.textContent = '$' + this.totalPerPerson(tip) 
+        }
+    },
+
+    totalTip(tip) { // get total tip
+        let result = ( (+tip / 100) * Number(bill.value) )
+        return result.toFixed(2)
+    },
+    tipPerPerson(tip) { //get tip per person 
+        let result = (this.totalTip(tip)) / (Number(numOfPeople.value))
+        return result.toFixed(2)
+    },
+    totalPerPerson(tip) { // get total amount per person
+        let totalAmount = Number(this.totalTip(tip)) + Number(bill.value)
+        let result = totalAmount / Number(numOfPeople.value)
+        return result.toFixed(2)
+    }
+}
+
+document.querySelector('.resetBtn').addEventListener('click', () => {
+    displayTip.textContent = '$0.00'
+    displayTotal.textContent = '$0.00'
+    bill.value = 0
+    numOfPeople.value = 0
+    customTip.value = ''
+    document.querySelector('.errorBill').textContent = ''
+    document.querySelector('.errorPeople').textContent = ''
+})
+
+
+
+
+
+
+
